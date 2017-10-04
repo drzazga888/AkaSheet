56e60d4d1cf0cced4d4463f89f31b54c57b4455e
Kamil Drzazga <kdrzazga@akamai.com>
2017-10-05 00:07:47 +0200

    routes init, some new files

---
+++ client/src/app.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
+
+import Layout from './layout'
+import HomePage from './pages/home'
+import SignInPage from './pages/sign-in'
+import SignUpPage from './pages/sign-up'
+import ReceiptsPage from './pages/receipts'
+import SummaryPage from './pages/summary'
+import NotFoundPage from './pages/not-found'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware(reduxThunk)))
+
+const App = () => (
+    <Provider store={store}>
+        <Router basename="webj2">
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/sign-in" component={SignInPage} />
+                    <Route path="/sign-up" component={SignUpPage} />
+                    <Route path="/receipts" component={ReceiptsPage} />
+                    <Route path="/summary" component={SummaryPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
+)
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,6 @@
-alert('hheuheuheue')
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+
+ReactDOM.render(<App />, document.getElementById('root'))
